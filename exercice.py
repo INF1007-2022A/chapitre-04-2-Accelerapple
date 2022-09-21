472ce88e7e6dc5ecfb8a4b82bb5a75984df1e41b
mberg <javier.berger@polymtl.com>
2022-09-21 00:47:31 -0400

    commit 1

--- exercice.py
+++ exercice.py
@@ -4,16 +4,34 @@
 import random
 
 def get_first_part_of_name(name):
-	return ""
+	premier_nom = name.split("-")
+	premier_nom = premier_nom[0].lower().capitalize()
+	return premier_nom
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	animal = random.choice(animals)
+	adjectif = random.choice(adjectives)
+	fruit = random.choice(fruits)
+	return f"Aujourd’hui, j’ai vu un {animal} s’emparer d’un panier {adjectif} plein de {fruit}."
 
 def encrypt(text, shift):
-	return ""
+	resultat = ""
+	text_maj = text.upper()
+	for letter in text_maj:
+		if letter.isalpha():
+			letter_chiffre = ord(letter)
+			letter_chiffre += shift
+			if letter_chiffre > 90:
+				letter_chiffre -= 26
+			elif letter_chiffre < 65:
+				letter_chiffre += 26
+			letter = chr(letter_chiffre)
+		resultat += letter
+	return resultat
 
 def decrypt(encrypted_text, shift):
-	return ""
+	not_chiffre = -shift
+	return encrypt(encrypted_text, not_chiffre)
 
 
 if __name__ == "__main__":
